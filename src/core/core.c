ebace9b31e257d98f614fd3bb2be8e043277193b
Armin Krezović <krezovic.armin@gmail.com>
2017-08-11 15:56:59 +0200

    Move xids and alarm stuff to MetaX11Display

--- src/core/core.c
+++ src/core/core.c
@@ -51,7 +51,7 @@ get_window (Display *xdisplay,
   MetaWindow *window;
 
   display = meta_display_for_x_display (xdisplay);
-  window = meta_display_lookup_x_window (display, frame_xwindow);
+  window = meta_x11_display_lookup_x_window (display->x11_display, frame_xwindow);
 
   if (window == NULL || window->frame == NULL)
     {
